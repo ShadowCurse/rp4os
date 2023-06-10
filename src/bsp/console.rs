2b7ec2295db3f61cc5e97bbd7b30b68e80d52f4f
ShadowCurse <shadowcurse57@gmail.com>
2023-06-10 18:23:08 +0100

    receive a kernel and boot it

--- src/bsp/console.rs
+++
@@ -1,5 +0,0 @@
-use crate::console;
-
-pub fn console() -> &'static impl console::interface::Console {
-    &super::driver::PL011_UART
-}
--- src/bsp/mod.rs
+++ src/bsp/mod.rs
@@ -1,7 +1,6 @@
-pub mod drivers;
-pub mod console;
 pub mod cpu;
 pub mod driver;
+pub mod drivers;
 pub mod memory;
 
 pub fn board_name() -> &'static str {
