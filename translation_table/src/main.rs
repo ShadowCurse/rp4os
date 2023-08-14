c62ce1968e9f79a6cd45e460b3c93ea74d527cae
ShadowCurse <shadowcurse57@gmail.com>
2023-08-14 23:12:22 +0100

    kernel segments to mapping descriptors

--- translation_table/src/main.rs
+++ translation_table/src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use elf::segment::ProgramHeader;
 use elf::string_table::StringTable;
 use elf::symbol::Symbol;
 use elf::ElfBytes;
@@ -32,6 +33,9 @@ fn main() {
     println!(
         "virt_addr_of_phys_kernel_tables_base_addr: {virt_addr_of_phys_kernel_tables_base_addr:#x}"
     );
+
+    let descriptors = map_kernel_binary(&file);
+    println!("{:#?}", descriptors);
 }
 
 fn get_symbol_value(
@@ -45,4 +49,104 @@ fn get_symbol_value(
         }
     }
     unreachable!("could not find {name}");
+}
+
+fn map_kernel_binary(file: &ElfBytes<AnyEndian>) -> Vec<MappingDescriptor> {
+    file.segments()
+        .unwrap()
+        .iter()
+        .filter(|segment| true)
+        .map(|segment| {
+            let size = align_up(segment.p_memsz, 64 * 1024);
+            let virt_start_addr = segment.p_vaddr;
+            let phys_start_addr = segment.p_paddr;
+            let acc_perms = match (segment.readable(), segment.writable()) {
+                (true, true) => AccessPermissions::ReadWrite,
+                (true, false) => AccessPermissions::ReadOnly,
+                _ => unreachable!(),
+            };
+            let execute_never = !segment.executable();
+
+            let virt_region = MemoryRegion {
+                start: virt_start_addr,
+                end_exclusive: virt_start_addr + size,
+            };
+            let phys_region = MemoryRegion {
+                start: phys_start_addr,
+                end_exclusive: phys_start_addr + size,
+            };
+            let attributes = AttributeFields {
+                mem_attributes: MemAttributes::CacheableDRAM,
+                acc_perms,
+                execute_never,
+            };
+
+            MappingDescriptor {
+                virt_region,
+                phys_region,
+                attributes,
+            }
+        })
+        .collect()
+}
+
+trait Segment {
+    fn readable(&self) -> bool;
+    fn writable(&self) -> bool;
+    fn executable(&self) -> bool;
+}
+
+impl Segment for ProgramHeader {
+    fn readable(&self) -> bool {
+        (self.p_flags & 4) == 4
+    }
+
+    fn writable(&self) -> bool {
+        (self.p_flags & 2) == 2
+    }
+
+    fn executable(&self) -> bool {
+        (self.p_flags & 1) == 1
+    }
+}
+
+/// Align up.
+#[inline(always)]
+pub const fn align_up(ptr: u64, alignment: u64) -> u64 {
+    assert!(alignment.is_power_of_two());
+
+    (ptr + alignment - 1) & !(alignment - 1)
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq)]
+pub enum AccessPermissions {
+    ReadOnly,
+    ReadWrite,
+}
+
+/// A type that describes a region of memory in quantities of pages.
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq)]
+pub struct MemoryRegion {
+    start: u64,
+    end_exclusive: u64,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq)]
+struct MappingDescriptor {
+    virt_region: MemoryRegion,
+    phys_region: MemoryRegion,
+    attributes: AttributeFields,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq)]
+pub struct AttributeFields {
+    pub mem_attributes: MemAttributes,
+    pub acc_perms: AccessPermissions,
+    pub execute_never: bool,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq)]
+pub enum MemAttributes {
+    CacheableDRAM,
+    Device,
 }
